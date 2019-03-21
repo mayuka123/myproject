12a3bb8b775a3ed912891f40b11164c74abb3e70
mayuka123 <48802551+mayuka123@users.noreply.github.com>
2019-03-21 23:38:59 +0530

    Update Test1111.java

--- mysampleproject/src/mysampleproject/Test1111.java
+++ mysampleproject/src/mysampleproject/Test1111.java
@@ -3,7 +3,7 @@ package mysampleproject;
 public class Test1111 {
 
 	public static void main(String[] args) {
-		System.out.println("my first git project");
+		System.out.println("my first git project");//print output to console
 		System.out.println("changes made in project");
 
 	}
